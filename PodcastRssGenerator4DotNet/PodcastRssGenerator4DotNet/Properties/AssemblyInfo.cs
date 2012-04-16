8079a6cc45f4794240d2e5f677a0f515443e7e34
Keyvan Nayyeri <nayyeri@gmail.com>
2012-04-16 15:22:54 -0500

    Updated assembly version to 1.5.

--- PodcastRssGenerator4DotNet/PodcastRssGenerator4DotNet/Properties/AssemblyInfo.cs
+++ PodcastRssGenerator4DotNet/PodcastRssGenerator4DotNet/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PodcastRssGenerator4DotNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Podcast RSS generator for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Keyvan Nayyeri")]
 [assembly: AssemblyProduct("PodcastRssGenerator4DotNet")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Keyvan Nayyeri 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
